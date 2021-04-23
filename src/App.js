977ba6b7577585f91813dfc8c13aa2d49380bace
Sarah Verbelen <sarah.verbelen@studiohyperdrive.be>
2021-04-23 13:56:43 +0200

    feat: why section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './modules/header/Header';
 import Landing from './modules/landing/Landing';
 import About from './modules/about/About';
+import Why from './modules/why/Why';
 import Footer from './modules/footer/Footer';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Landing />
       <About />
+      <Why />
       <Footer />
     </div>
   );
---
+++ src/modules/why/Why.js
@@ -0,0 +1,17 @@
+import './Why.css';
+
+function Why() {
+	return (
+		<section id='why'>
+			<h2>Waarom de plugin installeren?</h2>
+			<ul>
+				<li>Leer meer over hoe jij sociale media gebruikt</li>
+				<li>Krijg inzicht in de invloed die sociale media op jou heeft</li>
+				<li>Ontdek hoe je je ervaring op sociale media kan verbeteren</li>
+				<li>Verbreed je zelfkennis</li>
+			</ul>
+		</section>
+	)
+}
+
+export default Why;
