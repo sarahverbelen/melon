60d0752d2d50fc044e73069b4a46b0686f110ef8
Sarah Verbelen <sarah.verbelen@studiohyperdrive.be>
2021-04-29 11:01:16 +0200

    feat: new landingpage

--- src/modules/landing/Landing.js
+++ src/modules/landing/Landing.js
@@ -1,16 +1,30 @@
 import './Landing.css';
 
-import illustration from '../../img/landing-illustration.png';
-import Button from '../button/Button';
+import illustration1 from '../../img/landing-illustration-1.png';
+import illustration2 from '../../img/landing-illustration-2.png';
+import illustration3 from '../../img/landing-illustration-3.png';
+import arrow from '../../img/arrow.svg';
 
 function Landing() {
 	return (
 		<section className="Landing" data-testid='landing'>
-			<img src={illustration} alt='' data-testid='landingImage' />
-			<div className='innerLanding' data-testid='innerLanding'>
-				<p>Ontdek wat sociale media met je doet en haal het meeste uit je digitale leven</p>
-				<Button label='Installeer de plugin' url='https://google.com' position='center'></Button>
+			<div class='innerLanding'>
+			<div class='landingStep'>
+			<img src={illustration1} alt='' data-testid='landingImage1' />
+			<p>Soms kan sociale media je slecht of ongelukkig doen voelen...</p>
 			</div>
+			<img src={arrow} class='arrow' alt=''/>
+			<div class='landingStep'>
+			<img src={illustration2} alt='' data-testid='landingImage2' />
+			<p>Leer aan de hand van grafieken waar de positieve en negatieve effecten van jouw sociale media liggen.</p>
+			</div>
+			<img src={arrow} class='arrow' alt=''/>
+			<div class='landingStep'>
+			<img src={illustration3} alt='' data-testid='landingImage3' />
+			<p>Zo kan je je focussen op de positieve effecten en je gelukkiger voelen!</p>
+			</div>
+			</div>
+	
 			<div className='navigationButtons' data-testid='navigationButtons'>
 				<a href='#about'>Over Melon</a>
 				<a href='#why'>Waarom de plugin installeren?</a>
