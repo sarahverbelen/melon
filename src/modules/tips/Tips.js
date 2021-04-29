71d6ed1966b9b5b4e31434ba434b6c61cec4bea6
Sarah Verbelen <sarah.verbelen@studiohyperdrive.be>
2021-04-29 15:00:22 +0200

    wip: responsiveness

--- src/modules/tips/Tips.js
+++ src/modules/tips/Tips.js
@@ -8,19 +8,19 @@ function Tips() {
 				<div id='line'></div>
 				<div className='timeline-tip'>
 					<div className='tip-number'>1</div>
-					<p className="smallText"><b>Wees je ervan bewust dat niet alles wat op sociale media staat écht is.</b> Foto's worden bewerkt, verhalen opgeblazen en je ziet enkel wat iemand wil dat je ziet. Wat je van het leven ziet op sociale media kan je onmogelijk vergelijken met het échte leven.</p>
+					<p className="smallText"><b>Wees je ervan bewust dat niet alles wat op sociale media staat écht is.</b> <br /> Foto's worden bewerkt, verhalen opgeblazen en je ziet enkel wat iemand wil dat je ziet. Wat je van het leven ziet op sociale media kan je onmogelijk vergelijken met het échte leven.</p>
 				</div>
 				<div className='timeline-tip'>
 					<div className='tip-number'>2</div>
-					<p className="smallText"><b>Laat je inspireren.</b> Nee, niet alles wat op sociale media staat is realistisch. Maar het kan een goed begin zijn om je te laten inspireren, bijvoorbeeld over een nieuwe hobby of een nieuwe kledingstijl. Sociale media is enorm gevarieerd: maak daar gebruik van!</p>
+					<p className="smallText"><b>Laat je inspireren.</b> <br /> Nee, niet alles wat op sociale media staat is realistisch. Maar het kan een goed begin zijn om je te laten inspireren, bijvoorbeeld over een nieuwe hobby of een nieuwe kledingstijl. Sociale media is enorm gevarieerd: maak daar gebruik van!</p>
 				</div>
 				<div className='timeline-tip'>
 					<div className='tip-number'>3</div>
-					<p className="smallText"><b>Staar je niet blind op likes en volgers.</b> Weinig volgers of likes hebben betekend niet dat je posts slecht zijn. Populariteit op sociale media is vaak het gevolg van geluk. Straf jezelf dus niet af als je bericht het minder goed doet dan je gehoopt had.</p>
+					<p className="smallText"><b>Staar je niet blind op likes en volgers.</b> <br /> Weinig volgers of likes hebben betekend niet dat je posts slecht zijn. Populariteit op sociale media is vaak het gevolg van geluk. Straf jezelf dus niet af als je bericht het minder goed doet dan je gehoopt had.</p>
 				</div>
 				<div className='timeline-tip'>
 					<div className='tip-number'>4</div>
-					<p className="smallText"><b>Wees kritisch.</b> Sociale media kan een ontzettend goede informatiebron zijn. Er is content van mensen uit alle lagen van de maatschappij. Maar het is belangrijk om kritisch te blijven: liegen op het internet is niet moeilijk.</p>
+					<p className="smallText"><b>Wees kritisch.</b> <br /> Sociale media kan een ontzettend goede informatiebron zijn. Er is content van mensen uit alle lagen van de maatschappij. Maar het is belangrijk om kritisch te blijven: liegen op het internet is niet moeilijk.</p>
 				</div>
 			</div>
 		</section>
