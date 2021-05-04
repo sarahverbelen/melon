36c6c628d6382f80d06e4bc71aec7f5b23449d5b
Sarah Verbelen <sarah.verbelen@studiohyperdrive.be>
2021-05-04 14:40:29 +0200

    feat: bar chat aantal berichten / dag

--- src/modules/graphs/Graphs.js
+++ src/modules/graphs/Graphs.js
@@ -5,19 +5,28 @@ import './Graphs.css'
 
 class Graphs extends Component {
 	doughnutChartRef = React.createRef();
+	barChartRef = React.createContext();
 
 	componentDidMount() {
 		const myDoughnutChartRef = this.doughnutChartRef.current.getContext("2d");
+		const myBarChartRef = this.barChartRef.current.getContext("2d");
 		createDoughnutChart([], myDoughnutChartRef);
+		createBarChart([], myBarChartRef);
 	}
 
 	render(){
 		return(
 		<section id='graphs'>
 			<h2>Grafieken</h2>
+			<div id='graphArea'>
+			<div id='perDay'>
+				<h3>Aantal berichten / dag</h3>
+				<canvas id='barChart' ref={this.barChartRef}></canvas>
+			</div>
 			<div id='total'>
 				<h3>Algemene verdeling</h3>
 				<canvas id='dougnutChart' ref={this.doughnutChartRef}></canvas>
+			</div>
 			</div>
 		</section>
 		);
@@ -50,8 +59,60 @@ function createDoughnutChart(sentimentData, myDoughnutChartRef) {
         data: data
     }
 
-    let chart = new Chart(
+    new Chart(
         myDoughnutChartRef,
+        config
+    );
+}
+
+function createBarChart(sentimentData, myBarChartRef) {
+	const labels = [1, 2, 3, 4, 5, 6, 7]; // TODO: figure out a way to make these dates
+	const data = {
+	  labels: labels,
+	  datasets: [{
+		label: 'Positief',
+		data: [65, 59, 80, 81, 56, 55, 40], // TODO: fill with real data
+		backgroundColor: [
+			'rgb(89, 161, 96)'
+		],
+		borderWidth: 0
+	  },
+	  {
+		label: 'Neutraal',
+		data: [2, 10, 30, 5, 80, 15, 3], // TODO: fill with real data
+		backgroundColor: [
+			'rgb(103, 103, 103)'
+		],
+		borderWidth: 0
+	  },
+	  {
+		label: 'Negatief',
+		data: [25, 45, 0, 72, 30, 25, 39], // TODO: fill with real data
+		backgroundColor: [
+			'rgb(235, 98, 86)'
+		],
+		borderWidth: 0
+	  }]
+	};
+
+	const config = {
+		type: 'bar',
+		data: data,
+		options: {
+		  scales: {
+			y: {
+			  beginAtZero: true,
+			  stacked: true
+			},
+			x: {
+				stacked: true
+			}
+		  }
+		},
+	  };
+
+	  new Chart(
+        myBarChartRef,
         config
     );
 }
