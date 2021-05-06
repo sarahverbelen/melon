f9f7071cab17834e9ae126d238ca0e7789bd67e9
Sarah Verbelen <sarah.verbelen@studiohyperdrive.be>
2021-05-06 11:40:02 +0200

    feat: filter dropdown

--- src/modules/graphs/Graphs.js
+++ src/modules/graphs/Graphs.js
@@ -10,6 +10,20 @@ import reddit from '../../img/reddit.svg';
 
 
 class Graphs extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			filter: ''
+		}
+
+		this.handleFilter = this.handleFilter.bind(this);
+		this.getResults = this.getResults.bind(this);
+
+		this.createDoughnutChart = this.createDoughnutChart.bind(this);
+		this.createBartChart = this.createBarChart.bind(this);
+		this.createWebsiteChart = this.createWebsiteChart.bind(this);
+	}
+
 	// CREATE THE CHARTS
 	doughnutChartRef = React.createRef();
 	barChartRef = React.createRef();
@@ -17,28 +31,64 @@ class Graphs extends Component {
 	redditChartRef = React.createRef();
 	twitterChartRef = React.createRef();
 
+	doughnutChart;
+	barChart;
+	facebookChart;
+	redditChart;
+	twitterChart;
+
 	componentDidMount() {
+		this.getResults();
+	}
+
+	getResults() {
 		const myDoughnutChartRef = this.doughnutChartRef.current.getContext("2d");
 		const myBarChartRef = this.barChartRef.current.getContext("2d");
 		const myFacebookChartRef = this.facebookChartRef.current.getContext("2d");
 		const myRedditChartRef = this.redditChartRef.current.getContext("2d");
 		const myTwitterChartRef = this.twitterChartRef.current.getContext("2d");
 
+		let queryString = '';
+		let today = new Date();
+
+		if (this.state.filter === 'today') {
+			queryString = '';
+		} else if (this.state.filter === 'month') {
+			queryString = 'month=' + (today.getMonth() + 1)
+		}
+
+		console.log(queryString);
+
 		axios({
 			method: 'get',
-			url: 'http://127.0.0.1:5000/user/608fb0824832f22bdd3542f1/record/',
+			url: 'http://127.0.0.1:5000/user/608fb0824832f22bdd3542f1/record/?' + queryString,
 		})
 		.then(function(response) {
 			console.log(response.data);
-			createDoughnutChart(response.data, myDoughnutChartRef);
-			createBarChart(response.data, myBarChartRef);
-			createWebsiteChart(response.data, myFacebookChartRef, 'facebook');
-			createWebsiteChart(response.data, myRedditChartRef, 'reddit');
-			createWebsiteChart(response.data, myTwitterChartRef, 'twitter');
-		})
+			this.createDoughnutChart(response.data, myDoughnutChartRef);
+			this.createBarChart(response.data, myBarChartRef);
+			this.createWebsiteChart(response.data, myFacebookChartRef, 'facebook', this.facebookChart);
+			this.createWebsiteChart(response.data, myRedditChartRef, 'reddit', this.redditChart);
+			this.createWebsiteChart(response.data, myTwitterChartRef, 'twitter', this.twitterChart);
+		}.bind(this))
 		.catch(function(response) {
 			console.log(response);
 		});
+	}
+
+	handleFilter() {
+		this.setState({filter: document.getElementById("filter").value});
+		console.log(this.state.filter);
+		this.clearCharts();
+		this.getResults();
+	}
+
+	clearCharts() {
+		this.doughnutChart.destroy();
+		this.barChart.destroy();
+		this.facebookChart.destroy();
+		this.redditChart.destroy();
+		this.twitterChart.destroy();
 	}
 
 	// RENDER
@@ -46,6 +96,11 @@ class Graphs extends Component {
 		return(
 		<section id='graphs'>
 			<h2>Grafieken</h2>
+			<select name="filter" id="filter" onChange={this.handleFilter}>
+						<option value='today'>Vandaag</option>
+						<option value='month'>Deze maand</option>
+						<option value='year'>Dit jaar</option>
+					</select>
 			<div id='graphArea'>
 				<div id='perWebsite'>
 					<h3>Emotionele verdeling / website</h3>
@@ -76,154 +131,165 @@ class Graphs extends Component {
 		</section>
 		);
 	}
-}
 
-// DOUGHNUT CHART (ALGEMENE VERDELING)
-function createDoughnutChart(sentimentData, myDoughnutChartRef) {
-    const data = {
-        labels: [
-            'Positief',
-            'Neutraal',
-            'Negatief'
-        ],
-        datasets: [{
-            label: 'sentiment',
-            data: [sentimentData.positiveCount, sentimentData.neutralCount, sentimentData.negativeCount],
-            backgroundColor: [
-                'rgb(89, 161, 96)', // positive
-                'rgb(103, 103, 103)', // neutral
-                'rgb(235, 98, 86)' // negative
-            ],
-            hoverOffset: 2,
+	// DOUGHNUT CHART (ALGEMENE VERDELING)
+	createDoughnutChart(sentimentData, myDoughnutChartRef) {
+		const data = {
+			labels: [
+				'Positief',
+				'Neutraal',
+				'Negatief'
+			],
+			datasets: [{
+				label: 'sentiment',
+				data: [sentimentData.positiveCount, sentimentData.neutralCount, sentimentData.negativeCount],
+				backgroundColor: [
+					'rgb(89, 161, 96)', // positive
+					'rgb(103, 103, 103)', // neutral
+					'rgb(235, 98, 86)' // negative
+				],
+				hoverOffset: 2,
+				borderWidth: 0
+			}]
+		};
+
+		const config = {
+			type: 'doughnut',
+			data: data,
+		}
+
+		this.doughnutChart = new Chart(
+			myDoughnutChartRef,
+			config
+		);
+	}
+
+	// BAR CHART (AANTAL BERICHTEN PER DAG)
+	createBarChart(sentimentData, myBarChartRef) {
+		const labels = [1, 2, 3, 4, 5, 6, 7]; // TODO: figure out a way to make these dates
+		const data = {
+		labels: labels,
+		datasets: [{
+			label: 'Positief',
+			data: [65, 59, 80, 81, 56, 55, 40], // TODO: fill with real data
+			backgroundColor: [
+				'rgb(89, 161, 96)'
+			],
 			borderWidth: 0
-        }]
-    };
+		},
+		{
+			label: 'Neutraal',
+			data: [2, 10, 30, 5, 80, 15, 3], // TODO: fill with real data
+			backgroundColor: [
+				'rgb(103, 103, 103)'
+			],
+			borderWidth: 0
+		},
+		{
+			label: 'Negatief',
+			data: [25, 45, 0, 72, 30, 25, 39], // TODO: fill with real data
+			backgroundColor: [
+				'rgb(235, 98, 86)'
+			],
+			borderWidth: 0
+		}]
+		};
 
-    const config = {
-        type: 'doughnut',
-        data: data,
-    }
-
-    new Chart(
-        myDoughnutChartRef,
-        config
-    );
-}
-
-// BAR CHART (AANTAL BERICHTEN PER DAG)
-function createBarChart(sentimentData, myBarChartRef) {
-	const labels = [1, 2, 3, 4, 5, 6, 7]; // TODO: figure out a way to make these dates
-	const data = {
-	  labels: labels,
-	  datasets: [{
-		label: 'Positief',
-		data: [65, 59, 80, 81, 56, 55, 40], // TODO: fill with real data
-		backgroundColor: [
-			'rgb(89, 161, 96)'
-		],
-		borderWidth: 0
-	  },
-	  {
-		label: 'Neutraal',
-		data: [2, 10, 30, 5, 80, 15, 3], // TODO: fill with real data
-		backgroundColor: [
-			'rgb(103, 103, 103)'
-		],
-		borderWidth: 0
-	  },
-	  {
-		label: 'Negatief',
-		data: [25, 45, 0, 72, 30, 25, 39], // TODO: fill with real data
-		backgroundColor: [
-			'rgb(235, 98, 86)'
-		],
-		borderWidth: 0
-	  }]
-	};
-
-	const config = {
-		type: 'bar',
-		data: data,
-		options: {
-		  scales: {
-			y: {
-			  beginAtZero: true,
-			  stacked: true,
-			  grid: {
-				  display: false
-			  }
-			},
-			x: {
+		const config = {
+			type: 'bar',
+			data: data,
+			options: {
+			scales: {
+				y: {
+				beginAtZero: true,
 				stacked: true,
 				grid: {
 					display: false
 				}
+				},
+				x: {
+					stacked: true,
+					grid: {
+						display: false
+					}
+				}
 			}
-		  }
-		},
-	  };
-
-	  new Chart(
-        myBarChartRef,
-        config
-    );
-}
-
-// HORIZONTAL BAR CHARTS (EMOTIONELE VERDELING PER WEBSITE)
-function createWebsiteChart(sentimentData, myFacebookChartRef, website) {
-	const data = {
-	  labels: [''],
-	  datasets: [{
-		label: 'Positief',
-		data: [sentimentData.websiteCount[website].positive], 
-		backgroundColor: [
-			'rgb(89, 161, 96)'
-		],
-		borderWidth: 0
-	  },
-	  {
-		label: 'Neutraal',
-		data: [sentimentData.websiteCount[website].neutral], 
-		backgroundColor: [
-			'rgb(103, 103, 103)'
-		],
-		borderWidth: 0
-	  },
-	  {
-		label: 'Negatief',
-		data: [sentimentData.websiteCount[website].negative], 
-		backgroundColor: [
-			'rgb(235, 98, 86)'
-		],
-		borderWidth: 0
-	  }]
-	};
-
-	const config = {
-		type: 'bar',
-		data: data,
-		options: {
-		  indexAxis: 'y',
-		  scales: {
-			y: {
-			  beginAtZero: true,
-			  grid: {
-				  display: false
-			  }
 			},
-			x: {
+		};
+
+		this.barChart = new Chart(
+			myBarChartRef,
+			config
+		);
+	}
+
+	// HORIZONTAL BAR CHARTS (EMOTIONELE VERDELING PER WEBSITE)
+	createWebsiteChart(sentimentData, myChartRef, website) {
+		const data = {
+		labels: [''],
+		datasets: [{
+			label: 'Positief',
+			data: [sentimentData.websiteCount[website].positive], 
+			backgroundColor: [
+				'rgb(89, 161, 96)'
+			],
+			borderWidth: 0
+		},
+		{
+			label: 'Neutraal',
+			data: [sentimentData.websiteCount[website].neutral], 
+			backgroundColor: [
+				'rgb(103, 103, 103)'
+			],
+			borderWidth: 0
+		},
+		{
+			label: 'Negatief',
+			data: [sentimentData.websiteCount[website].negative], 
+			backgroundColor: [
+				'rgb(235, 98, 86)'
+			],
+			borderWidth: 0
+		}]
+		};
+
+		const config = {
+			type: 'bar',
+			data: data,
+			options: {
+			indexAxis: 'y',
+			scales: {
+				y: {
+				beginAtZero: true,
 				grid: {
 					display: false
 				}
+				},
+				x: {
+					grid: {
+						display: false
+					}
+				}
 			}
-		  }
-		},
-	  };
+			},
+		};
 
-	  new Chart(
-        myFacebookChartRef,
-        config
-    );
+		if(website === 'facebook') {
+			this.facebookChart = new Chart(
+				myChartRef,
+				config
+			);
+		} else if(website === 'reddit') {
+			this.redditChart = new Chart(
+				myChartRef,
+				config
+			);
+		} else if(website === 'twitter') {
+			this.twitterChart = new Chart(
+				myChartRef,
+				config
+			);
+		}
+	}
 }
-
 export default Graphs;
